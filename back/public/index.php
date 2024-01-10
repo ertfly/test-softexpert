0e0a81bc9444c8389f71a5c359b7ad4031b52b71
Eric Teixeira <ertfly@gmail.com>
2024-01-10 17:33:16 -0300

    add index start app

--- back/public/index.php
+++ back/public/index.php
@@ -1,1 +1,10 @@
-teste
+<?php
+
+use Helpers\DatabaseHelper;
+use Helpers\RouterHelper;
+
+require_once getenv('PATH_VENDOR');
+
+RouterHelper::start();
+//After that line do not insert any code
+DatabaseHelper::closeInstance();
