b490910b0eeec56b3b864143ca9521f8f974a5f4
Eric Teixeira <ertfly@gmail.com>
2024-01-12 10:58:54 -0300

    add new routes test

--- back/routes.php
+++ back/routes.php
@@ -1,12 +1,16 @@
 <?php
 
 return [
+    \App\Middlewares\TokenMiddleware::class => [
+        \App\Modules\AuthController::class => [
+            'get:/auth' => 'get',
+            'post:/auth' => 'post',
+            'delete:/auth' => 'delete',
+        ],
+    ],
     \App\Middlewares\NoTokenMiddleware::class => [
         \App\Modules\TokenController::class => [
             'post:/token' => 'post'
-        ],
-        \App\Modules\DashboardController::class => [
-            'get:/' => 'index'
         ],
     ],
 ];
