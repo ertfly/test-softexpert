76756a773008f88973e3deb5ed524dd8d6736164
Eric Teixeira <ertfly@gmail.com>
2024-01-12 11:09:41 -0300

    add empty dashboard for main page after logged

---
+++ front/src/user/modules/dashboard/containers/Dashboard.js
@@ -0,0 +1,30 @@
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+let Dashboard = () => {
+    return (
+        <div className="container-fluid p-4">
+            <div className="d-flex justify-content-between">
+                <div>
+                    <h2 className="card-title">Dashboard</h2>
+                    <p className="card-text">Resumo de vendas.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = ({ find: { searchWarnings } }) => ({
+    myResults: searchWarnings.list,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    methods: bindActionCreators(
+        {
+            
+        },
+        dispatch
+    )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
