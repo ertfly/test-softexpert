0530eed45d438e5e574403567e79dbae89ae1441
Eric Teixeira <ertfly@gmail.com>
2024-01-12 10:16:11 -0300

    add post token for navigation front

--- back/app/modules/TokenController.php
+++ back/app/modules/TokenController.php
@@ -2,9 +2,20 @@
 
 namespace App\Modules;
 
+use Helpers\SessionHelper;
+use Helpers\StringHelper;
+
 class TokenController
 {
     public function post(){
+        $token = StringHelper::token();
+        SessionHelper::create($token);
+        SessionHelper::init('api', $token);
+        SessionHelper::data('accessIp', $_SERVER['REMOTE_ADDR'] ?? null);
+        SessionHelper::data('accessBrowser', $_SERVER['HTTP_USER_AGENT'] ?? null);
 
+        responseApi([
+            'token' => $token,
+        ]);
     }
 }
