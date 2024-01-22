1e84046d151137cb564f709cbac97265b0e5bf00
Eric Teixeira <ertfly@gmail.com>
2024-01-22 01:38:12 -0300

    fix

--- back/tests/Helpers/SessionHelperTest.php
+++ back/tests/Helpers/SessionHelperTest.php
@@ -2,7 +2,36 @@
 
 namespace Tests\Helpers;
 
-class SessionHelperTest
+use Helpers\SessionHelper;
+use Helpers\StringHelper;
+use PHPUnit\Framework\TestCase;
+
+class SessionHelperTest extends TestCase
 {
-    
+    public function testShouldInitializeSession()
+    {
+        $token = StringHelper::token();
+        SessionHelper::create($token);
+        SessionHelper::init('app', $token);
+        SessionHelper::destroy();
+
+        $this->assertTrue(true);
+    }
+
+    public function testShouldSetDataInSession()
+    {
+        $token = StringHelper::token();
+        SessionHelper::create($token);
+        SessionHelper::init('app', $token);
+        SessionHelper::data('name', 'John Doe');
+
+        
+        $test = false;
+        if(SessionHelper::item('name') == 'John Doe') {
+            $test = true;
+        }
+        $this->assertTrue($test);
+
+        SessionHelper::destroy();
+    }
 }
